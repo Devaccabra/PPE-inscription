29c15f8157b717a4372ecda0528ec512b7a1d5ab
kkrane <cloarec.j95@gmail.com>
2017-02-03 14:31:05 +0100

    Add files via upload

--- inscriptions.sql
+++ inscriptions.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 20, 2017 at 09:26 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Client :  127.0.0.1
+-- Généré le :  Ven 03 Février 2017 à 13:30
+-- Version du serveur :  5.7.11
+-- Version de PHP :  5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,71 +14,126 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `inscriptions`
+-- Base de données :  `inscriptions`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `candidat`
+-- Structure de la table `candidat`
 --
 
-CREATE TABLE IF NOT EXISTS `candidat` (
-  `id_c` int(11) NOT NULL AUTO_INCREMENT,
-  `id_e` int(11) NOT NULL,
+CREATE TABLE `candidat` (
+  `id_c` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `candidat`
+--
+
+INSERT INTO `candidat` (`id_c`, `nom`) VALUES
+(3, '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `competition`
+--
+
+CREATE TABLE `competition` (
+  `id_comp` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `composer_equipe`
+--
+
+CREATE TABLE `composer_equipe` (
+  `id_equipe` int(11) NOT NULL,
+  `id_p` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participer`
+--
+
+CREATE TABLE `participer` (
+  `id_c` int(11) NOT NULL,
+  `id_comp` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personne`
+--
+
+CREATE TABLE `personne` (
+  `id_p` int(11) NOT NULL,
   `prenom` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_c`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `mail` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `candidat`
+-- Index pour les tables exportées
 --
 
-INSERT INTO `candidat` (`id_c`, `id_e`, `prenom`, `nom`) VALUES
-(1, 2, 'Jean', 'Reno'),
-(2, 8, 'Therry', 'Crews');
-
--- --------------------------------------------------------
+--
+-- Index pour la table `candidat`
+--
+ALTER TABLE `candidat`
+  ADD PRIMARY KEY (`id_c`);
 
 --
--- Table structure for table `equipe`
+-- Index pour la table `competition`
 --
-
-CREATE TABLE IF NOT EXISTS `equipe` (
-  `id_e` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
-  `id_s` int(11) NOT NULL,
-  PRIMARY KEY (`id_e`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+ALTER TABLE `competition`
+  ADD PRIMARY KEY (`id_comp`);
 
 --
--- Table structure for table `sport`
+-- Index pour la table `composer_equipe`
 --
-
-CREATE TABLE IF NOT EXISTS `sport` (
-  `id_s` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_s`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+ALTER TABLE `composer_equipe`
+  ADD PRIMARY KEY (`id_equipe`,`id_p`),
+  ADD KEY `id_p` (`id_p`);
 
 --
--- Table structure for table `tournoi`
+-- Index pour la table `participer`
+--
+ALTER TABLE `participer`
+  ADD KEY `id_c` (`id_c`),
+  ADD KEY `id_comp` (`id_comp`);
+
+--
+-- Index pour la table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id_p`),
+  ADD KEY `id_p` (`id_p`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
 --
 
-CREATE TABLE IF NOT EXISTS `tournoi` (
-  `id_t` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_t`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
+--
+-- AUTO_INCREMENT pour la table `candidat`
+--
+ALTER TABLE `candidat`
+  MODIFY `id_c` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `competition`
+--
+ALTER TABLE `competition`
+  MODIFY `id_comp` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
